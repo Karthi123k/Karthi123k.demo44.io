c7c4fb1f2d8385743e52f757854b79528961baed
Karthi123k <70147804+Karthi123k@users.noreply.github.com>
2025-06-06 22:14:14 +0530

    Create fotter.js
    Signed-off-by: Karthi123k <70147804+Karthi123k@users.noreply.github.com>

---
+++ fotter.js
@@ -0,0 +1,17 @@
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+  let currentPage = window.location.pathname.split("/").pop();
+  if (!currentPage) currentPage = "index.html";
+
+  currentPage = currentPage.split("?")[0].split("#")[0];
+
+  document.querySelectorAll(".navbar-nav .nav-link").forEach(link => {
+    let linkHref = link.getAttribute("href").split("/").pop();
+    linkHref = linkHref.split("?")[0].split("#")[0];
+
+    if (linkHref === currentPage) {
+      link.classList.add("active");
+    }
+  });
+});
+</script>
